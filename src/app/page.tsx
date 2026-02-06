f1fc2f893f882fd6504f7e1621b31818bc6eb8ad
MOHAMMED ADNAN PASHA <adnan.pasha@zyptr.com>
2026-02-06 11:08:06 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-5xl font-bold">Mohammed Adnan Pasha 🚀 ✨</h1>
+      <h1 className="text-5xl font-bold">Mohammed Adnan Pasha 🚀 </h1>
     </main>
   );
 }
